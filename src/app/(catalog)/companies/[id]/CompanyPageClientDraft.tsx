31f7eb36eb08bd6ae521da0f2c98172ed09d3410
victoria <v.suprun1961@gmail.com>
2026-06-24 10:50:09 +0300

    fix: fix page company (среда, июня 24, 2026 10:50:09)

--- src/app/(catalog)/companies/[id]/CompanyPageClientDraft.tsx
+++ src/app/(catalog)/companies/[id]/CompanyPageClientDraft.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 
 import { routes } from '@/config/routes';
-import { ReviewForm } from '@/components/company/ReviewForm';
+// import { ReviewForm } from '@/components/company/ReviewForm';
 import { buttonVariants } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { RatingStarsDisplay } from '@/components/ui/RatingStars';
@@ -12,6 +12,9 @@ import { cn } from '@/lib/cn';
 import type { Company } from '@/types/company';
 import type { Review } from '@/types/review';
 import Image from 'next/image';
+import { getCategoryIconSrc } from '@/lib/catalog-api/categoryIcon';
+import { CatalogOrganization  } from '@/types/catalog-api';
+
 
 type Props = {
   company: Company;
@@ -49,15 +52,19 @@ export default function CompanyPageClient({
           href={routes.home}
           className={cn(
             buttonVariants({ variant: 'ghost', size: 'sm' }),
-            'px-0 text-black',
+            'px-0 text-[#666666]',
           )}
         >
           {t('companyPage.back')}
-          <span>/ Продукти і супермаркети /</span>
-          {/* {t('companyPage.back')} */}
-          <span>АТБ-Маркет №679</span>
-          {/* {t('companyPage.back')} */}
         </Link>
+        <span> / </span>
+        <Link href={`/?categoryId=${company.categoryId}`}>
+          {company.primaryCategoryName}
+        </Link>
+        <span> / </span>
+        <span className="font-eUkraine text-[13px] leading-[18px] font-bold text-black">
+          {company.name}
+        </span>
       </div>
 
       <Card className="border-0 p-0">
@@ -65,11 +72,17 @@ export default function CompanyPageClient({
           {/* LEFT BLOCK */}
           <div className="shrink-0">
             <div className="mb-3 flex h-[176px] w-[176px] items-center justify-center rounded-full bg-gray-300 text-5xl font-bold text-gray-700">
-              {company.name?.charAt(0).toUpperCase()}
+              <Image
+                src={getCategoryIconSrc(company.categoryId)}
+                alt={company.category}
+                width={48}
+                height={48}
+              />
+              {/* {company.name?.charAt(0).toUpperCase()} */}
             </div>
             <p className="font-eUkraine text-center text-sm text-[13px] leading-[18px] font-normal">
-              {/* {company.category} */}
-              Мережа супермаркетів
+              {company.category}
+              {/* Мережа супермаркетів */}
             </p>
           </div>
 
@@ -79,20 +92,44 @@ export default function CompanyPageClient({
             <div className="flex min-h-[742px] w-full max-w-[884px] flex-col gap-4 rounded-[20px] bg-white">
               <div className="flex h-auto min-h-[196px] w-full max-w-[884px] flex-col gap-5 rounded-[20px] bg-[#E7E7E7] p-10">
                 <div className="space-y-2">
-                  <h1 className="font-eUkraine font-regular mb-2 text-[28px] leading-[32px] text-black">
-                    {company.name}
-                  </h1>
+                  <div className="flex items-center justify-between">
+                    <h1 className="font-eUkraine mb-2 text-[28px] leading-[32px] font-semibold text-black">
+                      {company.name}
+                    </h1>
+                    <div>
+                      <Image
+                        src="/assets/icons/share.svg"
+                        alt="Phone"
+                        width={32}
+                        height={32}
+                      />
+                    </div>
+                  </div>
                   <div className="mb-5 flex items-center gap-1">
-                    <Image
+                    {/* <Image
                       src="/assets/icons/location_on.svg"
                       alt="Location"
                       width={24}
                       height={24}
-                    />
-                    <p className="font-eUkraine text-[20px] leading-[24px] font-normal text-black">
-                      вул. Свято-Миколаївська, 44/13
-                      {/* {t('companyPage.sections.description')} */}
-                    </p>
+                    /> */}
+                    <div className="font-eUkraine text-[20px] leading-[24px] font-normal text-black">
+                      {/* ФРАГМЕНТ, ЕСЛИ НУЖНО ВЫВОДИТЬ ВСЕ АДРЕСА */}
+
+                      {company.addresses.map((addr, index) => (
+                        <div key={index} className="flex items-center gap-2">
+                          <Image
+                            src="/assets/icons/location_on.svg"
+                            alt="Location"
+                            width={24}
+                            height={24}
+                          />
+                          <p className="font-eUkraine text-[20px] leading-[24px] font-regular text-black">
+                            {addr}
+                          </p>
+                        </div>
+                      ))}
+                      {/* {company.primaryAddress} */}
+                    </div>
                   </div>
 
                   <div className="flex h-[32px] w-[185px] items-center gap-2 rounded-[20px] border border-black py-[7px] pr-[12px] pl-[8px]">
@@ -103,8 +140,8 @@ export default function CompanyPageClient({
                       height={16}
                     />
                     <p className="font-eUkraine text-[13px] leading-[18px] font-normal text-black">
-                      Відчинено до 23:00
-                      {/* {t('companyPage.sections.description')} */}
+                      {/* Відчинено до */}
+                      {company.workingHours}
                     </p>
                   </div>
                 </div>
@@ -113,22 +150,22 @@ export default function CompanyPageClient({
               <div className="flex h-auto min-h-[196px] w-full max-w-[884px] flex-col gap-2 rounded-[20px] bg-[#E7E7E7] p-10">
                 <div className="flex flex-col gap-2 space-y-3">
                   <h2 className="text-xl font-semibold">
-                    Про організацію
-                    {/* {t('companyPage.sections.description')} */}
+                    {/* Про організацію */}
+                    {t('companyPage.sections.description')}
                   </h2>
 
                   <p className="font-eUkraine mb-6 text-[16px] leading-[24px] font-light text-black">
+                    {/* {company.shortDescription} */}
                     Мережа АТБ надає не лише базові послуги з роздрібної
                     торгівлі, а й пропонує зручну цифрову екосистему. Основні
                     послуги охоплюють доставку, онлайн-замовлення, спеціальні
                     банківські програми, послуги на касі та мобільні сервіси.
-                    {/* {t('companyPage.sections.description')} */}
                   </p>
                 </div>
                 <div className="mb-6 flex flex-col gap-2 space-y-3">
                   <h2 className="mb-0 text-xl font-semibold">
                     Графік роботи
-                    {/* {t('companyPage.sections.description')} */}
+                    {/* {t('companyPage.sections.schedule')} */}
                   </h2>
                   <div className="flex items-center gap-2">
                     <Image
@@ -138,8 +175,9 @@ export default function CompanyPageClient({
                       height={24}
                     />
                     <p className="font-eUkraine text-[16px] leading-[24px] font-light text-black">
-                      Пн-Нд: 05:00 - 23:00
-                      {/* {t('companyPage.sections.description')} */}
+                      {/* Пн-Нд: */}
+                      {company.workingHours}
+                      {/* 05:00 - 23:00 */}
                     </p>
                   </div>
                 </div>
@@ -157,11 +195,20 @@ export default function CompanyPageClient({
                     <div className="flex flex-col">
                       <p className="font-eUkraine text-[16px] leading-[24px] font-light text-black">
                         0-800-50-04-15
-                        {/* {company.contacts?.phone} */}
+                        {/* {company.contacts?.phone && (
+                          <div className="flex items-center gap-2">
+                            <a
+                              href={`https://${company.contacts.phone}`}
+                              target="_blank"
+                              className="hover:underline"
+                            >
+                              {company.contacts.p}
+                            </a>
+                          </div>
+                        )} */}
                       </p>
                       <p className="font-eUkraine text-black-400 text-[13px] leading-[18px] font-light">
                         Дзвінки приймаються з 09:00 до 17:00
-                        {/* {t('companyPage.sections.description')} */}
                       </p>
                     </div>
                   </div>
@@ -174,10 +221,20 @@ export default function CompanyPageClient({
                     />
                     <p className="font-eUkraine text-[16px] leading-[24px] font-light text-black">
                       atb.office@atbmarket.com
-                      {/* {company.contacts?.email} */}
+                      {/* {company.contacts?.email && (
+                        <div className="flex items-center gap-2">
+                          <a
+                            href={`https://${company.contacts.email}`}
+                            target="_blank"
+                            className="hover:underline"
+                          >
+                            {company.contacts.email}
+                          </a>
+                        </div>
+                      )} */}
                     </p>
                   </div>
-                  <div className="flex flex-col gap-3 text-sm text-gray-600 mb-6">
+                  <div className="mb-6 flex flex-col gap-3 text-sm text-gray-600">
                     {company.contacts?.website && (
                       <div className="flex items-center gap-2">
                         <svg
@@ -214,7 +271,6 @@ export default function CompanyPageClient({
                         height={32}
                       />
                     </div>
-
                     <div>
                       <Image
                         src="/assets/icons/linkedin.svg"
@@ -223,7 +279,6 @@ export default function CompanyPageClient({
                         height={32}
                       />
                     </div>
-
                     <div>
                       <Image
                         src="/assets/icons/telegram.svg"
